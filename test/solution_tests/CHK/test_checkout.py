b36e3627a98b9317c431b90eec6c1a2b18706a1b
sbx_user1051 <sbx_user1051@169.254.102.253>
2024-06-22 14:08:23 +0000

    Sat Jun 22 14:08:23 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,4 +1,20 @@
+import pytest
+
 from lib.solutions.CHK import checkout_solution
 
-def test_checkout():
-    
+@pytest.mark.parametrize("skus, expected_total", [
+    ("", 0),
+    ("A", 50),
+    ("AA", 100),
+    ("AAA", 130),
+    ("AAAB", 160),
+    ("B", 30),
+    ("BB", 45),
+    ("ABCD", 115),
+    ("AAABB", 175),
+    ("AAAAAABBBB", 350),
+    ("XYZ", -1),
+    ("AAA1",-1)
+])
+def test_checkout(skus, expected_total):
+    assert checkout_solution.checkout(skus) == expected_total
